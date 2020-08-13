4b34b3b0e609fcaa75a916e594cb3967ae03b6a0
sosaheri <sosaheriberto2001@gmail.com>
2020-08-13 12:29:41 -0400

    feat: Create migrations.

---
+++ twgroup/database/migrations/2020_08_13_154633_create_comments_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('publication_id')->unsigned();
+            $table->string('content');
+            $table->string('status');
+            $table->foreign('publication_id')->references('id')->on('publications')->onDelete('cascade');
+            $table->bigInteger('user_id')->unsigned()->unique();
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
